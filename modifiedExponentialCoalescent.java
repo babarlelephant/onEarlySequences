f542a235b2b293a271d219cf451a7246776f6b60
babarlelephant <acx01b@yahoo.fr>
2022-06-19 04:12:42 +0200

    Update modifiedExponentialCoalescent.java

--- modifiedExponentialCoalescent.java
+++ modifiedExponentialCoalescent.java
@@ -4,13 +4,10 @@ public class modifiedCoalescent extends Coalescent {
 	// number of cases by onset from December 8 to January 23
 	int [] onsetCurve = new int[] {1,0,0,1,4,0,0,4,1,7,3,2,12,6,11,13,12,13,7,26,11,16,24,16,73,63,65,46,78,64,71,
 							125,117,187,168,214,281,296,404,458,572,653,667,1254,1222,1552,1937};
-	 							
+	int day0 = 32+25;  // January 25 (latest sequence in the dataset)	 							
 	
 	double serialInterval = 5/365.0; // 5 days
-
-	int day0 = 32+25;  // January 25 (latest sequence in the dataset)
 	double ascertainmentRate = 0.15; // divide by 0.15 the number of cases in the epidemic curve
-	double weight = 0.001;
 
 	@Override
 	public double calculateLogLikelihood(IntervalList intervals, PopulationFunction popSizeFunction, double threshold) {
@@ -24,7 +21,7 @@ public class modifiedCoalescent extends Coalescent {
 			double modeledPopulationSize = effectivePopSize/serialInterval;
 			double expectancyOfAscertainedPopulationSize = modeledPopulationSize*ascertainmentRate;
 			double normalizedDiff = Math.abs(onsetCurve[j] - expectancyOfAscertainedPopulationSize) / Math.sqrt(expectancyOfAscertainedPopulationSize) / ascertainmentRate;
-			logL -= normalizedDiff * weight; // 1st order approximation
+			logL -= normalizedDiff; // 1st order approximation
 		}				
 		return logL+super.calculateLogLikelihood(intervals,popSizeFunction,threshold);
 	}	
